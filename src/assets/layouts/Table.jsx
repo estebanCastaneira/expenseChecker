98a86f982122b68d743e0d4a098bdbe7c092fd93
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-01 17:10:12 -0300

    a lot of progess today

--- src/assets/layouts/Table.jsx
+++ src/assets/layouts/Table.jsx
@@ -1,9 +1,11 @@
 import Edit from "../components/Edit";
 import Delete from "../components/Delete";
 import Money from "../components/Money";
+import Save from "../components/Save";
 import { useState } from "react";
 function Table({ expenses, setExpenses }) {
-  const [editing, setEditing] = useState({ aviable: false, id: "" });
+  const [editing, setEditing] = useState({ available: false, id: "" });
+  const [clicked, setClicked] = useState(false);
 
   const handleDelete = (e, id) => {
     const expensesUpdate = expenses.filter(
@@ -12,8 +14,11 @@ function Table({ expenses, setExpenses }) {
     setExpenses([...expensesUpdate]);
   };
   const handleEdit = (e, id) => {
-    if (e) {
-      setEditing({ aviable: !editing.aviable, id: id });
+    setClicked(!clicked);
+    if (clicked) {
+      setEditing({ available: !editing.available, id: id });
+    } else {
+      setEditing({ available: false, id: "" });
     }
   };
   const handlePaid = (e) => {
@@ -62,28 +67,31 @@ function Table({ expenses, setExpenses }) {
             <tr className="text-center" key={i}>
               <td className="p-2 table-cell border-2 dark:border-gray-600 relative">
                 {expense.expense}
-                {editing.aviable && editing.id === i && (
+                {editing.available && editing.id === i && (
                   <input
-                    className=" dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-5  w-5/6"
+                    className=" text-center dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-5  w-5/6"
                     type="text"
+                    defaultValue={expense.expense}
                   />
                 )}
               </td>
               <td className="p-2 table-cell border-2 dark:border-gray-600 relative">
                 ${expense.amount}
-                {editing.aviable && editing.id === i && (
+                {editing.available && editing.id === i && (
                   <input
-                    className=" dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-3  w-5/6"
-                    type="text"
+                    className="text-center dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-3  w-5/6"
+                    type="number"
+                    defaultValue={expense.amount}
                   />
                 )}
               </td>
               <td className="p-2 table-cell border-2 dark:border-gray-600 relative">
                 {expense.expiration}
-                {editing.aviable && editing.id === i && (
+                {editing.available && editing.id === i && (
                   <input
-                    className=" dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-5  w-5/6"
-                    type="text"
+                    className="text-center dark:bg-slate-700 border-2 dark:border-white rounded-lg absolute left-5  w-5/6"
+                    type="date"
+                    defaultValue={expense.expiration}
                   />
                 )}
               </td>
@@ -92,6 +100,7 @@ function Table({ expenses, setExpenses }) {
               </td>
               <td className="p-2 table-cell border-2 dark:border-gray-600">
                 <div className="flex justify-evenly">
+                  <Save editing={editing} id={i} />
                   <Edit handleEdit={handleEdit} id={i} />
                   <Money handlePaid={handlePaid} id={i} />
                   <Delete handleDelete={handleDelete} id={i} />
