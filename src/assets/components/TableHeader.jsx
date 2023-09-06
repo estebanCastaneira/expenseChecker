1a32c614bcd68fc222a3233532f22ea5c6464c02
estebanCastaneira <estebanc_b@hotmail.com>
2023-09-06 15:32:19 -0300

    Headers sorts the expenses

--- src/assets/components/TableHeader.jsx
+++ src/assets/components/TableHeader.jsx
@@ -1,45 +1,86 @@
+import { useState } from "react";
 function TableHeader({ expenses, setExpenses }) {
+  const [expSorted, setExpSorted] = useState(false);
+  const [amoSorted, setAmoSorted] = useState(false);
+  const [expiSorted, setExpiSorted] = useState(false);
+
   const handleExpense = () => {
+    if (!expSorted) {
+      const expensesSorted = expenses.sort((a, b) => {
+        const nameA = a.expense.toLowerCase();
+        const nameB = b.expense.toLowerCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+      setExpSorted(true);
+      return setExpenses([...expensesSorted]);
+    }
     const expensesSorted = expenses.sort((a, b) => {
       const nameA = a.expense.toLowerCase();
       const nameB = b.expense.toLowerCase();
       if (nameA < nameB) {
-        return -1;
+        return 1;
       }
       if (nameA > nameB) {
-        return 1;
+        return -1;
       }
       return 0;
     });
-    setExpenses([...expensesSorted]);
+    setExpSorted(false);
+    return setExpenses([...expensesSorted]);
   };
   const handleAmount = () => {
+    if (!amoSorted) {
+      const expensesSorted = expenses.sort((a, b) => {
+        const amountA = a.amount;
+        const amountB = b.amount;
+        return amountA - amountB;
+      });
+      setAmoSorted(true);
+      return setExpenses([...expensesSorted]);
+    }
     const expensesSorted = expenses.sort((a, b) => {
       const amountA = a.amount;
       const amountB = b.amount;
-      if (amountA < amountB) {
-        return -1;
-      }
-      if (amountA > amountB) {
-        return 1;
-      }
-      return 0;
+      return amountB - amountA;
     });
-    setExpenses([...expensesSorted]);
+    setAmoSorted(false);
+    return setExpenses([...expensesSorted]);
   };
   const handleExpiration = () => {
+    if (!expiSorted) {
+      const expensesSorted = expenses.sort((a, b) => {
+        const dateA = a.expiration;
+        const dateB = b.expiration;
+        if (dateA < dateB) {
+          return -1;
+        }
+        if (dateA > dateB) {
+          return 1;
+        }
+        return 0;
+      });
+      setExpiSorted(true);
+      return setExpenses([...expensesSorted]);
+    }
     const expensesSorted = expenses.sort((a, b) => {
       const dateA = a.expiration;
       const dateB = b.expiration;
       if (dateA < dateB) {
-        return -1;
+        return 1;
       }
       if (dateA > dateB) {
-        return 1;
+        return -1;
       }
       return 0;
     });
-    setExpenses([...expensesSorted]);
+    setExpiSorted(false);
+    return setExpenses([...expensesSorted]);
   };
   const handlePaid = () => {
     const expensesSorted = expenses.sort((a, b) => {
